bea1e283c4795fbdeacb79e2156dc58ff8496d1a
Cerruti <cerrutik@PC-B103-05.iut-valence.fr>
2015-10-01 17:21:57 +0200

    mise à jour

---
+++ StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Instruction.java
@@ -0,0 +1,23 @@
+package fr.iutvalence.info.m3105.stackmachine;
+
+public class Instruction
+{
+	private int operation;
+	private int[] tab;
+	
+	public Instruction(int operation, int[] tab) {
+		this.operation = operation;
+		this.tab = tab;
+	}
+
+	public Object getOpCode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int[] getParams() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
