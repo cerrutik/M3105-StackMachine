fe42453d25026e000083542f5cf757f4793fd131
Cerruti <cerrutik@PC-B123-05.iut-valence.fr>
2015-10-02 14:52:42 +0200

    mise à jour

--- StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Memory.java
+++ StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Memory.java
@@ -2,10 +2,11 @@ package fr.iutvalence.info.m3105.stackmachine;
 
 public class Memory {
 	
-	private int i,j;
+	private int adresseMemoire1,adresseMemoire2;
 
-	public Memory(int i, int j) {
-		// TODO Auto-generated constructor stub
+	public Memory(int adresseMemoire1, int adresseMemoire2) {
+		this.adresseMemoire1 = adresseMemoire1;
+		this.adresseMemoire2 = adresseMemoire2;
 	}
 
 }
